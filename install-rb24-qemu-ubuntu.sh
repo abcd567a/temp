f571c6379b47d0f1ed54f84d0a53e1c63687cc55
abcd567a <28452511+abcd567a@users.noreply.github.com>
2026-03-04 08:24:16 -0500

    Update install-rb24-qemu-ubuntu.sh

--- install-rb24-qemu-ubuntu.sh
+++ install-rb24-qemu-ubuntu.sh
@@ -4,10 +4,14 @@ set -e
 sudo apt update
 sudo apt -y install qemu-user qemu-user-binfmt binfmt-support
 
+echo "Adding line \"Architectures: amd64\" to existing file \"ubuntu.sources\", if not already there ..."
+sudo sed -i '/Architectures: amd64/d' /etc/apt/sources.list.d/ubuntu.sources
 sudo sed -i '/Types: deb/a Architectures: amd64' /etc/apt/sources.list.d/ubuntu.sources
+
+echo "Adding arhitecture arm64 to system ..."
 sudo dpkg --add-architecture arm64
 
-echo "Creating arm64 apt-source list file ubuntu-ports-arm64.sources"
+echo "Creating arm64 apt-source file \"ubuntu-ports-arm64.sources\"   "
 ARM64_SOURCES_FILE=/etc/apt/sources.list.d/ubuntu-ports-arm64.sources
 touch ${ARM64_SOURCES_FILE}
 chmod 777 ${ARM64_SOURCES_FILE}
@@ -37,9 +41,11 @@ libncurses6:arm64 \
 libprotobuf-c1:arm64 \
 libtinfo6:arm64
 
+echo "downloading and installing packge librtlsdr0:arm64 ..."
 wget -O librtlsdr0_0.6.0-4_arm64.deb http://http.us.debian.org/debian/pool/main/r/rtl-sdr/librtlsdr0_0.6.0-4_arm64.deb
 dpkg -i librtlsdr0_0.6.0-4_arm64.deb
 
+echo "downloading & installing rbfeeder:arm64 ..."
 wget -O rbfeeder_1.0.15+bookworm_arm64.deb https://apt.rb24.com/pool/main/r/rbfeeder/rbfeeder_1.0.15%2bbookworm_arm64.deb
 sudo dpkg -i rbfeeder_1.0.15+bookworm_arm64.deb || true
 sudo apt -y --fix-broken install
