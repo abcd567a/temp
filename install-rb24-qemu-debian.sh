b83ebf8c7bd78b78feda1b1612288e4d51b38a8c
abcd567a <28452511+abcd567a@users.noreply.github.com>
2026-03-05 17:24:44 -0500

    Update install-rb24-qemu-debian.sh
    Added descriptions in color

--- install-rb24-qemu-debian.sh
+++ install-rb24-qemu-debian.sh
@@ -1,24 +1,32 @@
 #!/bin/bash
 set -e
 
-sudo apt update
+sudo apt update || true
+
+echo -e "\e[1;32mInstalling packages to provide QEMU support ...\e[0;39m"
 sudo apt -y install qemu-user qemu-user-binfmt binfmt-support
 
+echo -e "\e[1;32mAdding arhitecture arm64 to system ...\e[0;39m"
 sudo dpkg --add-architecture arm64
 sudo apt update
+
+echo -e "\e[1;32mInstalling package libc6:arm64 to provide ar m64 support ...\e[0;39m"
 sudo apt -y install libc6:arm64
 
+echo -e "\e[1;32mSetting up RB24 repository \e[0;39m"
 sudo apt install dirmngr gnupg
 gpg --keyserver keyserver.ubuntu.com --recv-keys F2A8428D3C354953
 gpg --export --armor F2A8428D3C354953 | sudo gpg --dearmor -o /etc/apt/keyrings/rb24.gpg
 echo "deb [signed-by=/etc/apt/keyrings/rb24.gpg] https://apt.rb24.com/ bookworm main" | sudo tee /etc/apt/sources.list.d/rb24.list
 
 sudo apt update
+
+echo -e "\e[1;32mRunning command \"sudo apt install rbfeeder\" to nstalli rbfeeder:arm64 from RB24 repository ...\e[0;39m"
 sudo apt install rbfeeder
 
 sudo systemctl restart rbfeeder
 
-echo "Plese check your file /etc/rbfeeder.ini. I will contain your feeder key and station number"
-echo "Visit Radarbox Claims web page to claim the key and link station to yor account"
-echo "You must login to your Radarbox account when visisting Claims page"
+echo -e "\e[1;35mPlese check your file /etc/rbfeeder.ini. It will contain your feeder key and station number \e[0;39m"
+echo -e "\e[1;35mVisit Radarbox Claims web page to claim the key and link station to yor account \e[0;39m"
+echo -e "\e[1;35mYou must login to your Radarbox account when visisting Claims page \e[0;39m"
 
