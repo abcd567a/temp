83adab6c83d106428c09234ab97e9c3156ae932e
abcd567a <28452511+abcd567a@users.noreply.github.com>
2026-03-05 19:49:52 -0500

    Update install-rb24-qemu-ubuntu.sh

--- install-rb24-qemu-ubuntu.sh
+++ install-rb24-qemu-ubuntu.sh
@@ -36,7 +36,7 @@ echo -e "\e[1;32mInstalling package libc6:arm64 to provide ar m64 support ...\e[
 sudo apt -y install libc6:arm64
 
 echo -e "\e[1;32mSetting up RB24 repository \e[0;39m"
-sudo apt install dirmngr gnupg
+sudo apt install -y dirmngr gnupg
 gpg --keyserver keyserver.ubuntu.com --recv-keys F2A8428D3C354953
 gpg --export --armor F2A8428D3C354953 | sudo gpg --dearmor -o /etc/apt/keyrings/rb24.gpg
 echo "deb [signed-by=/etc/apt/keyrings/rb24.gpg] https://apt.rb24.com/ bookworm main" | sudo tee /etc/apt/sources.list.d/rb24.list
@@ -44,13 +44,14 @@ echo "deb [signed-by=/etc/apt/keyrings/rb24.gpg] https://apt.rb24.com/ bookworm 
 sudo apt update 
 
 echo -e "\e[1;32mRunning command \"sudo apt install rbfeeder\" to nstalli rbfeeder:arm64 from RB24 repository ...\e[0;39m"
-sudo apt install rbfeeder
+sudo apt install -y rbfeeder
 
 sudo systemctl restart rbfeeder
 
-echo "Plese check your file /etc/rbfeeder.ini. It will contain your feeder key and station number"
-echo "Visit Radarbox Claims web page to claim the key and link station to yor account"
-echo "You must login to your Radarbox account when visisting Claims page"
+echo " "
+echo -e "\e[1;35mPlese check your file \e[1;32m /etc/rbfeeder.ini \e" "\e[1;35m It will contain your feeder key and station number \e[0;39m"
+echo -e "\e[1;35mVisit Radarbox Claims web page to claim the key and link station to yor account \e[0;39m"
+echo -e "\e[1;35mYou must login to your Radarbox account when visisting Claims page \e[0;39m"
 
 
 
