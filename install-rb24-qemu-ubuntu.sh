288083dee8f10b9a3f0848b28d475c09ceef32fb
abcd567a <28452511+abcd567a@users.noreply.github.com>
2026-03-07 14:18:05 -0500

    Update install-rb24-qemu-ubuntu.sh

--- install-rb24-qemu-ubuntu.sh
+++ install-rb24-qemu-ubuntu.sh
@@ -1,24 +1,29 @@
 #!/bin/bash
 set -e
 
-sudo apt update || true
+OS_VER=`lsb_release -sc`
+
+apt update || true
 
 echo -e "\e[1;32mInstalling packages to provide QEMU support ...\e[0;39m"
-sudo apt -y install qemu-user qemu-user-binfmt binfmt-support
+apt -y install qemu-user qemu-user-binfmt binfmt-support
 
-echo -e "\e[1;32mAdding line \"Architectures: amd64\" to existing file \"ubuntu.sources\", if not already there ...\e[0;39m"
-sudo sed -i '/Architectures: amd64/d' /etc/apt/sources.list.d/ubuntu.sources
-sudo sed -i '/Types: deb/a Architectures: amd64' /etc/apt/sources.list.d/ubuntu.sources
 
 echo -e "\e[1;32mAdding arhitecture arm64 to system ...\e[0;39m"
-sudo dpkg --add-architecture arm64
+dpkg --add-architecture arm64
+
+
+if [[ ${OS_VER} == noble ]]; then
+echo -e "\e[1;32mAdding line \"Architectures: amd64\" to existing file \"ubuntu.sources\", if not already there ...\e[0;39m"
+sed -i '/Architectures: amd64/d' /etc/apt/sources.list.d/ubuntu.sources
+sed -i '/Types: deb/a Architectures: amd64' /etc/apt/sources.list.d/ubuntu.sources
 
 echo -e "\e[1;32mCreating arm64 apt-source file \"ubuntu-ports-arm64.sources\"  \e[0;39m"
-ARM64_SOURCES_FILE=/etc/apt/sources.list.d/ubuntu-ports-arm64.sources
-touch ${ARM64_SOURCES_FILE}
-chmod 777 ${ARM64_SOURCES_FILE}
+NOBLE_ARM64_SOURCES_FILE=/etc/apt/sources.list.d/ubuntu-ports-arm64.sources
+touch ${NOBLE_ARM64_SOURCES_FILE}
+chmod 777 ${NOBLE_ARM64_SOURCES_FILE}
 echo "Writing code to file ubuntu-ports-arm64.sources"
-/bin/cat <<EOM >${ARM64_SOURCES_FILE}
+/bin/cat <<EOM >${NOBLE_ARM64_SOURCES_FILE}
 Types: deb
 URIs: http://ports.ubuntu.com/ubuntu-ports/
 Suites: noble noble-updates noble-security
@@ -28,35 +33,71 @@ Signed-By: /usr/share/keyrings/ubuntu-archive-keyring.gpg
 
 EOM
 
-chmod 644 ${ARM64_SOURCES_FILE}
+chmod 644 ${NOBLE_ARM64_SOURCES_FILE}
 
-sudo apt update
+
+elif [[ ${OS_VER} == jammy ]]; then
+#echo -e "\e[1;32mAdding \"[arch=amd64]\" after \"deb\" to all lines in existing file \"sources.list\" ...\e[0;39m"
+#sed -i '/deb-src/!s/deb/deb [arch=amd64]/1' /etc/apt/sources.list
+
+echo -e "\e[1;32mCreating arm64 apt-source file \"ubuntu-ports-arm64.list\" ...\e[0;39m"
+JAMMY_ARM64_SOURCES_FILE=/etc/apt/sources.list.d/ubuntu-ports-arm64.list
+touch ${JAMMY_ARM64_SOURCES_FILE}
+chmod 777 ${JAMMY_ARM64_SOURCES_FILE}
+echo "Writing code to file ubuntu-ports-arm64.list"
+/bin/cat <<EOM >${JAMMY_ARM64_SOURCES_FILE}
+deb [arch=arm64] http://ports.ubuntu.com/ubuntu-ports jammy main restricted universe multiverse
+deb [arch=arm64] http://ports.ubuntu.com/ubuntu-ports jammy-updates main restricted universe multiverse
+deb [arch=arm64] http://ports.ubuntu.com/ubuntu-ports jammy-security main restricted universe multiverse
+
+EOM
+
+chmod 644 ${JAMMY_ARM64_SOURCES_FILE}
+
+fi
+
+apt update
 
 echo -e "\e[1;32mInstalling package libc6:arm64 to provide ar m64 support ...\e[0;39m"
-sudo apt -y install libc6:arm64
+apt -y install libc6:arm64
 
+if [[ ${OS_VER} == noble ]]; then
 echo -e "\e[1;32mDownloading & installing package \"librtlsdr0\" from Debian archieves ...\e[0;39m"
-sudo wget -O /tmp/librtlsdr0_0.6.0-4_arm64.deb http://http.us.debian.org/debian/pool/main/r/rtl-sdr/librtlsdr0_0.6.0-4_arm64.deb
-sudo apt install -y /tmp/librtlsdr0_0.6.0-4_arm64.deb
+wget -O /tmp/librtlsdr0_0.6.0-4_arm64.deb http://http.us.debian.org/debian/pool/main/r/rtl-sdr/librtlsdr0_0.6.0-4_arm64.deb
+apt install -y /tmp/librtlsdr0_0.6.0-4_arm64.deb
+fi
 
 echo -e "\e[1;32mSetting up RB24 repository \e[0;39m"
-sudo apt install -y dirmngr gnupg
+apt install -y dirmngr gnupg
 gpg --keyserver keyserver.ubuntu.com --recv-keys F2A8428D3C354953
 gpg --export --armor F2A8428D3C354953 | sudo gpg --dearmor -o /etc/apt/keyrings/rb24.gpg
+
+if [[ ${OS_VER} == noble ]]; then
 echo "deb [signed-by=/etc/apt/keyrings/rb24.gpg] https://apt.rb24.com/ bookworm main" | sudo tee /etc/apt/sources.list.d/rb24.list
+elif [[ ${OS_VER} == jammy ]]; then
+echo "deb [signed-by=/etc/apt/keyrings/rb24.gpg] https://apt.rb24.com/ bullseye main" | sudo tee /etc/apt/sources.list.d/rb24.list
+fi
 
-sudo apt update 
+apt update
 
-echo -e "\e[1;32mRunning command \"sudo apt install rbfeeder\" to nstalli rbfeeder:arm64 from RB24 repository ...\e[0;39m"
-sudo apt install -y rbfeeder
-sudo systemctl restart rbfeeder
+echo -e "\e[1;32mRunning command \"apt install rbfeeder\" to nstalli rbfeeder:arm64 from RB24 repository ...\e[0;39m"
+apt install -y rbfeeder
+systemctl restart rbfeeder
 
 echo -e "\e[1;32mDownloading & installing package \"mlat-client\" from github.com/abcd567a/ ...\e[0;39m"
-sudo apt install -y python3-pyasyncore
+if [[ ${OS_VER} == noble ]]; then
+apt install -y python3-pyasyncore
 wget -O /tmp/mlat-client_0.2.13_noble_amd64.deb https://github.com/abcd567a/rbfeeder/releases/download/v1.0/mlat-client_0.2.13_noble_amd64.deb || true
-sudo apt install -y /tmp/mlat-client_0.2.13_noble_amd64.deb || true
-sudo apt-mark hold mlat-client || true
-sudo systemctl restart rbfeeder
+apt install -y /tmp/mlat-client_0.2.13_noble_amd64.deb || true
+
+elif [[ ${OS_VER} == jammy ]]; then
+wget -O /tmp/mlat-client_0.2.13_jammy_amd64.deb https://github.com/abcd567a/rbfeeder/releases/download/v1.0/mlat-client_0.2.13_jammy_amd64.deb
+apt install -y /tmp/mlat-client_0.2.13_jammy_amd64.deb
+fi
+
+apt-mark hold mlat-client || true
+
+systemctl restart rbfeeder
 
 echo " "
 echo -e "\e[1;35mPlese check your file \e[1;32m /etc/rbfeeder.ini \e" "\e[1;35m It will contain your feeder key and station number \e[0;39m"
